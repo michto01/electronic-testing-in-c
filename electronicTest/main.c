ca4d4e320d8b788aabe0124cd158c30f6695b0df
Tomas Michalek <tomas.michalek@gatema.cz>
2015-01-15 02:03:35 +0100

    SCANF skips ... WHY!!! ???

--- electronicTest/main.c
+++ electronicTest/main.c
@@ -6,16 +6,18 @@
 //  Copyright (c) 2015 lucieurbanova.cz. All rights reserved.
 //
 
-#define N_DEBUG 1
+//#define N_DEBUG 0
 #define IMPORT_LOCATION "/Developer/gatema/Experiments/electronicTest/electronicTest/math.test"
 #define EXPORT_LOCATION "/Developer/gatema/Experiments/electronicTest/electronicTest/new.math.test"
 
 #include <stdio.h>
 #include "etests.h"
 
+Question* createQuestion();
+
 int main(int argc, const char * argv[]) {
     
-    int exit = FALSE;
+    //int exit = FALSE;
     int testLength;
     
     Question** test = extract(IMPORT_LOCATION, &testLength);
@@ -25,15 +27,30 @@ int main(int argc, const char * argv[]) {
     for ( int i = 0; i < testLength; i++ ) {
         printQuestion(test[i]);
     }
+    
+    Question** mock = (Question**) malloc( 2 * sizeof(Question*) );
+    mock[0] = createQuestion();
+    mock[1] = createQuestion();
+    
+    store(EXPORT_LOCATION, mock, 2);
 #endif
+    
+    /*
     printf("Select action: ");
+    
     do {
-        switch ( getchar() ) {
-            case 1:
-                getchar();
+        int ch = getchar(); getchar();
+        switch ( ch ) {
+            case '1':
                 if ( TRUE == store(EXPORT_LOCATION, test, testLength) ) {
                     printf("@Test exported");
                 }
+                break;
+                
+            case 2:
+                Question **mock;
+                = (Question**) malloc( 2 * sizeof(Question*) );
+                
                 break;
                 
             default:
@@ -41,7 +58,7 @@ int main(int argc, const char * argv[]) {
                 break;
         }
     } while ( exit != TRUE );
-    
+    */
     
     return 0;
 }
@@ -61,12 +78,13 @@ Question* createQuestion() {
     
     //DESCTIPTION
     printf(">> Type your question (max ~250 chars): ");
-    if ( NULL == (question->description = (char*) malloc(sizeof(char))) ) {
+    if ( NULL == (question->description = (char*) malloc(256 * sizeof(char))) ) {
         printf("Malloc failed\n");
         return NULL;
     }
-    scanf("%253s", question->description); // %253 scans only the specyfied lenght avoiding Undefined behaviour
-    
+    scanf("%254s[^n]", question->description); // %253 scans only the specyfied lenght avoiding Undefined behaviour
+    getchar();
+    getchar();
     
     //COUNT
     printf(">>> Specify number of answers for question: ");
@@ -84,7 +102,21 @@ Question* createQuestion() {
             return NULL;
         }
         
+        answer->id = i;
         
+        //ANSWER
+        printf("< Answer (~250 chars max): ");
+        if ( NULL == (answer->description = (char*) malloc(256 * sizeof(char))) ) {
+            printf("Malloc failed\n");
+            return NULL;
+        }
+        scanf("%254s", answer->description); // %253 scans only the specyfied length avoiding Undefined Behaviour
+        
+        //IS_CORRECT
+        printf("<< is answer correct [1:0] ?: "); ///FIXME: implemnet checking of type
+        scanf("%c", &answer->isCorrect);
+        
+        question->answers[i] = answer;
     }
     return question;
 }
@@ -103,8 +135,3 @@ void printQuestion ( Question* question ) {
     printf( "%s\n", question->description );
     printAnswers(question);
 }
-
-
-
-
-
